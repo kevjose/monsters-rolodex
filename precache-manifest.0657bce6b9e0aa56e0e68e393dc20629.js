d975beeded406f210031f12aaa30b5e4032bf674
Kevin Jose <kevin.josev@gmail.com>
2019-10-06 11:51:44 +0530

    Updates

---
+++ precache-manifest.0657bce6b9e0aa56e0e68e393dc20629.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c6c75d6957d562014148332b7f5af43",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6a7e75d54648862445af",
+    "url": "/monsters-rolodex/static/css/main.a6a253a9.chunk.css"
+  },
+  {
+    "revision": "4ee53b84b1f31b69fcd8",
+    "url": "/monsters-rolodex/static/js/2.01c3c59a.chunk.js"
+  },
+  {
+    "revision": "6a7e75d54648862445af",
+    "url": "/monsters-rolodex/static/js/main.68ad215c.chunk.js"
+  },
+  {
+    "revision": "61e64e548c9076aea34b",
+    "url": "/monsters-rolodex/static/js/runtime-main.c620570a.js"
+  }
+]);
