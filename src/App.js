56b64668bb738464590acd214fd24c3cf6b24a4a
Kevin Jose <kevin.josev@gmail.com>
2019-10-06 11:35:48 +0530

    adding files for github pages

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { CardList } from './components/card-list/card-llst.component';
+import { SearchBox } from './components/search-box/search-box.component';
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+    // this.handleChange = this.handleChange.bind(this); use this in case of class method and not arrow function
+  }
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+  handleChange = e => {
+    // arrow function takes the 'this' binding of the surrounding context
+    return this.setState({ searchField: e.target.value });
+  };
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        ></SearchBox>
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
